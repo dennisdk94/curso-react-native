1d0befd91f68e283635679593f98e4d2cef69f14
Dennis Dyodi Kawakami <denniskawakami@gmail.com>
2020-05-28 17:34:32 -0300

    Tasks back-end

---
+++ tasks-backend/migrations/20200528103815_create_table_tasks.js
@@ -0,0 +1,20 @@
+
+exports.up = function (knex) {
+  return knex.schema.createTable('tasks', table => {
+    table.increments('id').primary()
+    table.string('desc').notNull()
+    table.dateTime('estimateAt')
+    table.dateTime('doneAt')
+    table.integer('userId')
+      .references('id').inTable('users').notNull()
+  })
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('tasks')
+};
+
+/**
+ * knex migrate:latest
+ * knex migrate:rollback
+ */
